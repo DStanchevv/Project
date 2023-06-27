25fb293b3e96317d0028a420b6e553283a6d72de
D. Stanchev <denislavstanchev23@gmail.com>
2023-06-27 20:37:27 +0300

    seed data

--- SportWave/SportWave/Data/SportWaveDbContext.cs
+++ SportWave/SportWave/Data/SportWaveDbContext.cs
@@ -43,7 +43,316 @@ namespace SportWave.Data
             builder.Entity<PromoUser>().HasKey(x => new { x.UserId, x.PromoCodeId });
             builder.Entity<Order>().Property(x => x.OrderTotal).HasPrecision(18, 2);
             builder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2);
+
+            builder.Entity<PaymentType>().HasData(this.GeneratePaymentTypes());
+            builder.Entity<OrderStatus>().HasData(this.GenerateOrderStatus());
+            builder.Entity<ProductColor>().HasData(this.GenerateProductColor());
+            builder.Entity<ProductSize>().HasData(this.GenerateProductSize());
+            builder.Entity<ProductGender>().HasData(this.GenerateProductGender());
+            builder.Entity<ProductCategory>().HasData(this.GenerateProductCategory());
+            builder.Entity<PromoCode>().HasData(this.GeneratePromoCode());
+            builder.Entity<Product>().HasData(this.GenerateProduct());
+            builder.Entity<ProductVariation>().HasData(this.GenerateProductVariations());
+
             base.OnModelCreating(builder);
         }
+
+        private PaymentType[] GeneratePaymentTypes()
+        {
+            ICollection<PaymentType> paymentTypes = new HashSet<PaymentType>();
+
+            PaymentType paymentType;
+
+            paymentType = new PaymentType()
+            {
+                Id = 1,
+                Type = "Card"
+            };
+            paymentTypes.Add(paymentType);
+
+            paymentType = new PaymentType()
+            {
+                Id = 2,
+                Type = "Cash"
+            };
+            paymentTypes.Add(paymentType);
+
+            return paymentTypes.ToArray();
+        }
+
+        private OrderStatus[] GenerateOrderStatus()
+        {
+            ICollection<OrderStatus> orderStatuses = new HashSet<OrderStatus>();
+
+            OrderStatus orderStatus;
+
+            orderStatus = new OrderStatus()
+            {
+                Status = "On the way"
+            };
+            orderStatuses.Add(orderStatus);
+
+            orderStatus = new OrderStatus()
+            {
+                Status = "Shipped"
+            };
+            orderStatuses.Add(orderStatus);
+
+            return orderStatuses.ToArray();
+        }
+
+        private ProductColor[] GenerateProductColor()
+        {
+            ICollection<ProductColor> productColors = new HashSet<ProductColor>();
+
+            ProductColor productColor;
+
+            productColor = new ProductColor()
+            {
+                Color = "White"
+            };
+            productColors.Add(productColor);
+
+            productColor = new ProductColor()
+            {
+                Color = "Black"
+            };
+            productColors.Add(productColor);
+
+            productColor = new ProductColor()
+            {
+                Color = "Red"
+            };
+            productColors.Add(productColor);
+
+            return productColors.ToArray();
+        }
+
+        private ProductSize[] GenerateProductSize()
+        {
+            ICollection<ProductSize> productSizes = new HashSet<ProductSize>();
+
+            ProductSize productSize;
+
+            productSize = new ProductSize()
+            {
+                Size = "XS"
+            };
+            productSizes.Add(productSize);
+
+            productSize = new ProductSize()
+            {
+                Size = "S"
+            };
+            productSizes.Add(productSize);
+
+            productSize = new ProductSize()
+            {
+                Size = "M"
+            };
+            productSizes.Add(productSize);
+
+            productSize = new ProductSize()
+            {
+                Size = "L"
+            };
+            productSizes.Add(productSize);
+
+            productSize = new ProductSize()
+            {
+                Size = "XL"
+            };
+            productSizes.Add(productSize);
+
+            return productSizes.ToArray();
+        }
+
+        private ProductGender[] GenerateProductGender()
+        {
+            ICollection<ProductGender> productGenders = new HashSet<ProductGender>();
+
+            ProductGender productGender;
+
+            productGender = new ProductGender()
+            {
+                Gender = "Male"
+            };
+            productGenders.Add(productGender);
+
+            productGender = new ProductGender()
+            {
+                Gender = "Female"
+            };
+            productGenders.Add(productGender);
+
+            return productGenders.ToArray();
+        }
+
+        private ProductCategory[] GenerateProductCategory()
+        {
+            ICollection<ProductCategory> productCategories = new HashSet<ProductCategory>();
+
+            ProductCategory productCategory;
+
+            productCategory = new ProductCategory()
+            {
+                Id = 1,
+                Category = "T-Shirts"
+            };
+            productCategories.Add(productCategory);
+
+            productCategory = new ProductCategory()
+            {
+                Id = 2,
+                Category = "Hoodies"
+            };
+            productCategories.Add(productCategory);
+
+            productCategory = new ProductCategory()
+            {
+                Id = 3,
+                Category = "Shorts"
+            };
+            productCategories.Add(productCategory);
+
+            return productCategories.ToArray();
+        }
+
+        private PromoCode[] GeneratePromoCode()
+        {
+            ICollection<PromoCode> promoCodes = new HashSet<PromoCode>();
+
+            PromoCode promoCode;
+
+            promoCode = new PromoCode()
+            {
+                Code = "CODE10",
+                Value = 10,
+                isValid = true
+            };
+            promoCodes.Add(promoCode);
+
+            promoCode = new PromoCode()
+            {
+                Code = "CODE20",
+                Value = 20,
+                isValid = true
+            };
+            promoCodes.Add(promoCode);
+
+            return promoCodes.ToArray();
+        }
+
+        private Product[] GenerateProduct()
+        {
+            ICollection<Product> products = new HashSet<Product>();
+
+            Product product;
+
+            product = new Product()
+            {
+                Id = 1,
+                Name = "T-Shirt V1",
+                Price = 15.99m,
+                Description = "A very light, soft and comfortable T-shirt made of 100% cotton.",
+                CategoryId = 1,
+                ImgUrl = "/img/T-Shirt V1.jpg"
+            };
+            products.Add(product);
+
+            product = new Product()
+            {
+                Id = 2,
+                Name = "Hoodie V1",
+                Price = 20.99m,
+                Description = "A very light, soft and comfortable hoodie made of 100% cotton.",
+                CategoryId = 2,
+                ImgUrl = "/img/Hoodie V1.jpg"
+            };
+            products.Add(product);
+
+            product = new Product()
+            {
+                Id = 3,
+                Name = "Shorts V1",
+                Price = 20.99m,
+                Description = "A very light, soft and comfortable Shorts made of 100% cotton.",
+                CategoryId = 3,
+                ImgUrl = "/img/Shorts V1.jpg"
+            };
+            products.Add(product);
+
+            return products.ToArray();
+        }
+
+        private ProductVariation[] GenerateProductVariations()
+        {
+            ICollection<ProductVariation> productsVariations = new HashSet<ProductVariation>();
+
+            ProductVariation productVariation;
+
+            productVariation = new ProductVariation()
+            {
+                ProductId = 1,
+                Color = "White",
+                Gender = "Male",
+                Size = "S",
+                Quantity = 10
+            };
+            productsVariations.Add(productVariation);
+
+            productVariation = new ProductVariation()
+            {
+                ProductId = 1,
+                Color = "White",
+                Gender = "Male",
+                Size = "M",
+                Quantity = 10
+            };
+            productsVariations.Add(productVariation);
+
+            productVariation = new ProductVariation()
+            {
+                ProductId = 2,
+                Color = "White",
+                Gender = "Male",
+                Size = "S",
+                Quantity = 10
+            };
+            productsVariations.Add(productVariation);
+
+            productVariation = new ProductVariation()
+            {
+                ProductId = 2,
+                Color = "White",
+                Gender = "Male",
+                Size = "M",
+                Quantity = 10
+            };
+            productsVariations.Add(productVariation);
+
+            productVariation = new ProductVariation()
+            {
+                ProductId = 3,
+                Color = "Black",
+                Gender = "Male",
+                Size = "S",
+                Quantity = 10
+            };
+            productsVariations.Add(productVariation);
+
+            productVariation = new ProductVariation()
+            {
+                ProductId = 3,
+                Color = "Black",
+                Gender = "Male",
+                Size = "M",
+                Quantity = 10
+            };
+            productsVariations.Add(productVariation);
+
+            return productsVariations.ToArray();
+        }
+
     }
 }
---
+++ SportWave/SportWave/Migrations/20230627173514_dbSeed.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportWave.Migrations
+{
+    public partial class dbSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "OrderStatuses",
+                column: "Status",
+                values: new object[]
+                {
+                    "On the way",
+                    "Shipped"
+                });
+
+            migrationBuilder.InsertData(
+                table: "PaymentTypes",
+                columns: new[] { "Id", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Card" },
+                    { 2, "Cash" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "Category" },
+                values: new object[,]
+                {
+                    { 1, "T-Shirts" },
+                    { 2, "Hoodies" },
+                    { 3, "Shorts" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductColors",
+                column: "Color",
+                values: new object[]
+                {
+                    "Black",
+                    "Red",
+                    "White"
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductGenders",
+                column: "Gender",
+                values: new object[]
+                {
+                    "Female",
+                    "Male"
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductSizes",
+                column: "Size",
+                values: new object[]
+                {
+                    "L",
+                    "M",
+                    "S",
+                    "XL",
+                    "XS"
+                });
+
+            migrationBuilder.InsertData(
+                table: "PromoCodes",
+                columns: new[] { "Id", "Code", "Value", "isValid" },
+                values: new object[,]
+                {
+                    { new Guid("837707c8-7d9d-4274-9bdc-1168cfb4eacb"), "CODE10", 10, true },
+                    { new Guid("de9dc45e-ad66-404b-9e4c-501ac441af4f"), "CODE20", 20, true }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImgUrl", "Name", "Price" },
+                values: new object[] { 1, 1, "A very light, soft and comfortable T-shirt made of 100% cotton.", "/img/T-Shirt V1.jpg", "T-Shirt V1", 15.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImgUrl", "Name", "Price" },
+                values: new object[] { 2, 2, "A very light, soft and comfortable hoodie made of 100% cotton.", "/img/Hoodie V1.jpg", "Hoodie V1", 20.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImgUrl", "Name", "Price" },
+                values: new object[] { 3, 3, "A very light, soft and comfortable Shorts made of 100% cotton.", "/img/Shorts V1.jpg", "Shorts V1", 20.99m });
+
+            migrationBuilder.InsertData(
+                table: "ProductsVariations",
+                columns: new[] { "Color", "Gender", "ProductId", "Size", "Quantity" },
+                values: new object[,]
+                {
+                    { "White", "Male", 1, "M", 10 },
+                    { "White", "Male", 1, "S", 10 },
+                    { "White", "Male", 2, "M", 10 },
+                    { "White", "Male", 2, "S", 10 },
+                    { "Black", "Male", 3, "M", 10 },
+                    { "Black", "Male", 3, "S", 10 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "OrderStatuses",
+                keyColumn: "Status",
+                keyValue: "On the way");
+
+            migrationBuilder.DeleteData(
+                table: "OrderStatuses",
+                keyColumn: "Status",
+                keyValue: "Shipped");
+
+            migrationBuilder.DeleteData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductColors",
+                keyColumn: "Color",
+                keyValue: "Red");
+
+            migrationBuilder.DeleteData(
+                table: "ProductGenders",
+                keyColumn: "Gender",
+                keyValue: "Female");
+
+            migrationBuilder.DeleteData(
+                table: "ProductSizes",
+                keyColumn: "Size",
+                keyValue: "L");
+
+            migrationBuilder.DeleteData(
+                table: "ProductSizes",
+                keyColumn: "Size",
+                keyValue: "XL");
+
+            migrationBuilder.DeleteData(
+                table: "ProductSizes",
+                keyColumn: "Size",
+                keyValue: "XS");
+
+            migrationBuilder.DeleteData(
+                table: "ProductsVariations",
+                keyColumns: new[] { "Color", "Gender", "ProductId", "Size" },
+                keyValues: new object[] { "White", "Male", 1, "M" });
+
+            migrationBuilder.DeleteData(
+                table: "ProductsVariations",
+                keyColumns: new[] { "Color", "Gender", "ProductId", "Size" },
+                keyValues: new object[] { "White", "Male", 1, "S" });
+
+            migrationBuilder.DeleteData(
+                table: "ProductsVariations",
+                keyColumns: new[] { "Color", "Gender", "ProductId", "Size" },
+                keyValues: new object[] { "White", "Male", 2, "M" });
+
+            migrationBuilder.DeleteData(
+                table: "ProductsVariations",
+                keyColumns: new[] { "Color", "Gender", "ProductId", "Size" },
+                keyValues: new object[] { "White", "Male", 2, "S" });
+
+            migrationBuilder.DeleteData(
+                table: "ProductsVariations",
+                keyColumns: new[] { "Color", "Gender", "ProductId", "Size" },
+                keyValues: new object[] { "Black", "Male", 3, "M" });
+
+            migrationBuilder.DeleteData(
+                table: "ProductsVariations",
+                keyColumns: new[] { "Color", "Gender", "ProductId", "Size" },
+                keyValues: new object[] { "Black", "Male", 3, "S" });
+
+            migrationBuilder.DeleteData(
+                table: "PromoCodes",
+                keyColumn: "Id",
+                keyValue: new Guid("837707c8-7d9d-4274-9bdc-1168cfb4eacb"));
+
+            migrationBuilder.DeleteData(
+                table: "PromoCodes",
+                keyColumn: "Id",
+                keyValue: new Guid("de9dc45e-ad66-404b-9e4c-501ac441af4f"));
+
+            migrationBuilder.DeleteData(
+                table: "ProductColors",
+                keyColumn: "Color",
+                keyValue: "Black");
+
+            migrationBuilder.DeleteData(
+                table: "ProductColors",
+                keyColumn: "Color",
+                keyValue: "White");
+
+            migrationBuilder.DeleteData(
+                table: "ProductGenders",
+                keyColumn: "Gender",
+                keyValue: "Male");
+
+            migrationBuilder.DeleteData(
+                table: "ProductSizes",
+                keyColumn: "Size",
+                keyValue: "M");
+
+            migrationBuilder.DeleteData(
+                table: "ProductSizes",
+                keyColumn: "Size",
+                keyValue: "S");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
