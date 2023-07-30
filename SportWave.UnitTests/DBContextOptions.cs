a98814f5940d9bf327405889b116534e296a70f5
D. Stanchev <denislavstanchev23@gmail.com>
2023-07-30 15:48:19 +0300

    .

--- SportWave.UnitTests/DBContextOptions.cs
+++ SportWave.UnitTests/DBContextOptions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SportWave.Data;
 
 namespace SportWave.UnitTests
--- SportWave.UnitTests/MenAndWomenServiceTests.cs
+++ SportWave.UnitTests/MenAndWomenServiceTests.cs
@@ -2,15 +2,14 @@
 using SportWave.Data.Models;
 using SportWave.Services;
 using SportWave.Services.Contracts;
-using Xunit;
-using Assert = Xunit.Assert;
+using SportWave.ViewModels.MenAndWomenViewModels;
 
 namespace SportWave.UnitTests
 {
     public class MenAndWomenServiceTests
     {
         [Fact]
-        public async Task GetProductsAsyncDoesntReturnNull()
+        public async Task GetProductsAsyncDoesntReturnNullWhenThereIsProduct()
         {
             SportWaveDbContext context = new SportWaveDbContext(DbContextOptions.Options);
             IMenAndWomenService service = new MenAndWomenService(context);
@@ -41,8 +40,90 @@ namespace SportWave.UnitTests
             await context.SaveChangesAsync();
 
             Assert.NotNull(service.GetProductsAsync(1));
+
+            context.Dispose();
         }
 
-        
+        [Fact]
+        public async Task GetProductsAsyncDoesntReturnNullWhenThereIsNoProduct()
+        {
+            SportWaveDbContext context = new SportWaveDbContext(DbContextOptions.Options);
+            IMenAndWomenService service = new MenAndWomenService(context);
+
+            context.ProductGenders.Add(new ProductGender
+            {
+                Id = 1,
+                Gender = "test"
+            });
+            await context.SaveChangesAsync();
+
+            Assert.NotNull(service.GetProductsAsync(1));
+
+            context.Dispose();
+        }
+
+        [Fact]
+        public async Task GetFilteredProductsAsyncDoesntReturnNullWhenThereIsProduct()
+        {
+            SportWaveDbContext context = new SportWaveDbContext(DbContextOptions.Options);
+            IMenAndWomenService service = new MenAndWomenService(context);
+
+            context.ProductGenders.Add(new ProductGender
+            {
+                Id = 1,
+                Gender = "test"
+            });
+
+            context.ProductCategories.Add(new ProductCategory
+            {
+                Id = 1,
+                Category = "test"
+            });
+
+            context.Products.Add(new Product
+            {
+                Id = 1,
+                Name = "test",
+                Color = "test",
+                Price = 30m,
+                Description = "test test test test test test test test test test test test test test test test test test test",
+                CategoryId = 1,
+                GenderId = 1,
+                ImgUrl = "test"
+            });
+
+            AllProductsViewModel model = new AllProductsViewModel();
+            await context.SaveChangesAsync();
+
+            Assert.NotNull(service.GetFilteredProductsAsync(1, model));
+
+            context.Dispose();
+        }
+
+        [Fact]
+        public async Task GetFilteredProductsAsyncDoesntReturnNullWhenThereIsNoProduct()
+        {
+            SportWaveDbContext context = new SportWaveDbContext(DbContextOptions.Options);
+            IMenAndWomenService service = new MenAndWomenService(context);
+
+            context.ProductGenders.Add(new ProductGender
+            {
+                Id = 1,
+                Gender = "test"
+            });
+
+            context.ProductCategories.Add(new ProductCategory
+            {
+                Id = 1,
+                Category = "test"
+            });
+
+            AllProductsViewModel model = new AllProductsViewModel();
+            await context.SaveChangesAsync();
+
+            Assert.NotNull(service.GetFilteredProductsAsync(1, model));
+
+            context.Dispose();
+        }
     }
 }
