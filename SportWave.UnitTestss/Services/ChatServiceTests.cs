6ac56ec5491f3fdf63111548cbe6d70e94d61f65
D. Stanchev <denislavstanchev23@gmail.com>
2023-08-05 12:44:01 +0300

    change unit test

--- SportWave.UnitTestss/Services/ChatServiceTests.cs
+++ SportWave.UnitTestss/Services/ChatServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using SportWave.Data;
 using SportWave.Services.Contracts;
 using SportWave.Services;
+using SportWave.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportWave.UnitTestss.Services
 {
@@ -80,13 +82,33 @@ namespace SportWave.UnitTestss.Services
             SportWaveDbContext context = new SportWaveDbContext(DbContextOptions.Options);
             IChatService service = new ChatService(context);
 
+            context.Users.Add(new ApplicationUser
+            {
+                Id = Guid.Parse("591C3FC7-2E0A-498A-9693-713DC1C10DD9"),
+                UserName = "test@gmail.com",
+                NormalizedUserName = "TEST@GMAIL.COM",
+                Email = "test@gmail.com",
+                NormalizedEmail = "TEST@GMAIL.COM",
+                EmailConfirmed = false,
+                PasswordHash = "AQAAAAEAACcQAAAAEHyipAH79RA/Wg+CUvmeFxVsOIm2zlnApqzSeSEtHgKPARYIni9m+EyBMv5XjsGq5Q==",
+                SecurityStamp = "34CRR52GZU54BMGOKQLXNSZ3TE5VI7ZT",
+                ConcurrencyStamp = "88a2c0c0-8884-4045-a4e0-154b1d1cd30d",
+                PhoneNumber = null,
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnd = null,
+                LockoutEnabled = true,
+                AccessFailedCount = 0
+            });
+
             await context.SaveChangesAsync();
 
             await service.SaveMsgAsync("Test", "Test");
+            var user = await context.Users.Where(u => u.Id == Guid.Parse("591C3FC7-2E0A-498A-9693-713DC1C10DD9")).FirstOrDefaultAsync();
+            var userName = user.Email.Split("@").ToArray()[0];
+            var msgs = await service.GetAllMsgsAsync(userName);
 
-            var msgs = await service.GetAllMsgsAsync();
-
-            Assert.Single(msgs);
+            Assert.NotEmpty(msgs.Messages);
 
             context.Dispose();
         }
